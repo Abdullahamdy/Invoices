11d067b32ce6b411f81f0f01982b0c3df3a64f3a
Abdullahamdy <abdullahhamdy29@gmail.com>
2021-08-10 12:29:18 +0200

    inital commit

---
+++ app/products.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class products extends Model
+{
+    protected $guarded = [];
+
+    public function Section(){
+        return $this->belongsTo('App\sections','section_id');
+    }
+}
